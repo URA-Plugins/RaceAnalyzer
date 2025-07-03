dd986c08fbf94475db42104e828d4ae1ec843ade
离披 <lipi@shuise.dev>
2025-07-03 22:29:19 +0800

    null检测

--- Class1.cs
+++ Class1.cs
@@ -28,7 +28,7 @@ namespace RaceAnalyzer
 
         public void AnalyzeRoomMatch(dynamic dyn)
         {
-            if (dyn.data.race_scenario == null || dyn.data.random_seed == null || dyn.data.race_horse_data_array == null || dyn.data.trained_chara_array == null || dyn.data.season == null || dyn.data.weather == null || dyn.data.ground_condition == null)
+            if (dyn.data == null || dyn.data.race_scenario == null || dyn.data.random_seed == null || dyn.data.race_horse_data_array == null || dyn.data.trained_chara_array == null || dyn.data.season == null || dyn.data.weather == null || dyn.data.ground_condition == null)
                 return;
             var data = ((RoomMatchRaceStartResponse)dyn.ToObject<RoomMatchRaceStartResponse>()).data;
             Directory.CreateDirectory("races");
@@ -51,7 +51,7 @@ namespace RaceAnalyzer
         }
         public void AnalyzePracticeRace(dynamic dyn)
         {
-            if (dyn.data.trained_chara_array == null || dyn.data.race_result_info == null || dyn.data.entry_info_array == null || dyn.data.practice_race_id == null || dyn.data.state == null || dyn.data.practice_partner_owner_info_array == null)
+            if (dyn.data == null || dyn.data.trained_chara_array == null || dyn.data.race_result_info == null || dyn.data.entry_info_array == null || dyn.data.practice_race_id == null || dyn.data.state == null || dyn.data.practice_partner_owner_info_array == null)
                 return;
             var data = ((PracticeRaceRaceStartResponse)dyn.ToObject<PracticeRaceRaceStartResponse>()).data;
             Directory.CreateDirectory("races");
@@ -74,7 +74,7 @@ namespace RaceAnalyzer
         }
         public void AnalyzeChampionsRace(dynamic dyn)
         {
-            if (dyn.data.room_info == null || dyn.data.room_user_array == null || dyn.data.race_horse_data_array == null || dyn.data.trained_chara_array == null)
+            if (dyn.data == null || dyn.data.room_info == null || dyn.data.room_user_array == null || dyn.data.race_horse_data_array == null || dyn.data.trained_chara_array == null)
             {
                 return;
             }
